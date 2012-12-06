91afcc29a96dfe68ce2a6c505293a65068beab96
Novikov Andrey <andrnovi@green-book.local>
2012-12-06 14:04:03 +0200

    add work with period

--- core.rb
+++ core.rb
@@ -1,27 +1,28 @@
 RULE1 = {
   object: :credit_card,
-  #period: 2.hours,
+  period: 3, # only seconds
   operation: :greater_then,
   value: 5
 }
 
 RULE2 = {
   object: :credit_card,
-  #period: 2.hours,
+  period: 2,
   operation: :equal,
   value: 3
 }
 
 RULE3 = {
   object: :bad_password,
-  #period: 2.hours,
+  period: 10,
   operation: :greater_then,
   value: 4
 }
 
 RULES = [RULE1, RULE2, RULE3]
 
-$storage = Hash.new { |h,k| h[k] = 0 }
+$start_time = Time.now
+$storage    = Hash.new { |h,k| h[k] = 0 }
 
 # actions
 def greater_then(current_value, rule_value)
@@ -32,13 +33,17 @@ def equal(current_value, rule_value)
   current_value == rule_value
 end
 
+def check_period?(rule_period)
+  (Time.now - $start_time) <= rule_period
+end
+
 # server
 def request(object)
   $storage[object] += 1
 
   result = {}
   object_rules(object).each do |rule|
-    result[rule[:operation]] = send(rule[:operation], $storage[object], rule[:value])
+    result[rule[:operation]] = check_period?(rule[:period]) && send(rule[:operation], $storage[object], rule[:value])
   end
 
   result
@@ -50,8 +55,12 @@ end
 
 # test app
 objects = [:credit_card, :bad_password, :ip]
-(1..20).each do
-  object = objects.sample
+(1..20).each do |i|
+  sleep(1)
+  puts "sleep #{'.'*i} #{i}"
+
+  # object = objects.sample
+  object = :bad_password
   result = request(object)
   puts "Request '#{object}' is '#{result}'"
 end
