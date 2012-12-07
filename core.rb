79b579346515d3e724cf628677c9f8969ba2a71e
Mac Shifford <shiroginne@gmail.com>
2012-12-07 17:01:17 +0300

    add sinatra. add json response

--- core.rb
+++ core.rb
@@ -1,45 +1,20 @@
+require 'sinatra'
+require 'json'
+
 require File.expand_path(File.join(Dir.pwd, 'rules.rb'))
+require File.expand_path(File.join(Dir.pwd, 'server.rb'))
 
-$storage = Hash.new { |h,k| h[k] = [] }
-
-# actions
-def greater_then(current_value, rule_value)
-  current_value > rule_value
+configure :production, :development do
+  disable :logging
 end
 
-def equal(current_value, rule_value)
-  current_value == rule_value
-end
-
-# server
-def key_name(params, operation)
-  "#{params[:object]}_#{params[:value]}_#{operation}"
-end
-
-def request(params)
-  result = {}
-  
-  object_rules(params[:object]).each do |rule|
-    key = key_name(params, rule[:operation])
+get '/*' do
+  if params['object'].nil? || params['value'].nil?
+    'No params passed'
+  else
+    object = params['object'].to_sym
+    value = params['value']
     
-    $storage[key] << Time.now
-    $storage[key].delete_if {|time| (Time.now - rule[:period]) >= time } if rule[:period]
-
-    l_value = $storage[key].count
-
-    result[rule[:operation]] = send(rule[:operation], l_value, rule[:value])
+    server_request({ object: object, value: value }).to_json
   end
-
-  result
-end
-
-def object_rules(object)
-  RULES.find_all { |rule| rule[:object] == object }
-end
-
-# test app
-objects = [:credit_card, :bad_password, :ip]
-1.upto(2_000_000).each do |i|
-  
-  result = request({ object: :credit_card, value: i.to_s })
 end
---
+++ server.rb
@@ -0,0 +1,36 @@
+$storage = Hash.new { |h,k| h[k] = [] }
+
+# actions
+def greater_then(current_value, rule_value)
+  current_value > rule_value
+end
+
+def equal(current_value, rule_value)
+  current_value == rule_value
+end
+
+# server
+def key_name(params, operation)
+  "#{params[:object]}_#{params[:value]}_#{operation}"
+end
+
+def object_rules(object)
+  RULES.find_all { |rule| rule[:object] == object }
+end
+
+def server_request(params)
+  result = {}
+
+  object_rules(params[:object]).each do |rule|
+
+    key = key_name(params, rule[:operation])
+
+    $storage[key] << Time.now
+    $storage[key].delete_if {|time| (Time.now - rule[:period]) >= time } if rule[:period]
+
+    l_value = $storage[key].count
+
+    result[rule[:operation]] = send(rule[:operation], l_value, rule[:value])
+  end
+  result
+end
